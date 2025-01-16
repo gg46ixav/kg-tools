07007503ce09a5528ee944d72f9ac2533567740f
gg46ixav <gg46ixav@studserv.uni-leipzig.de>
2025-01-16 17:11:53 +0100

    Update run_docker_containers.sh

--- run_docker_containers.sh
+++ run_docker_containers.sh
@@ -14,7 +14,9 @@ for dir in "$BASE_DIR"/*/; do
         docker build -t "$IMAGE_NAME" "$dir"
         
         echo "Run Container: $CONTAINER_NAME"
-        docker run --name "$CONTAINER_NAME" -d "$IMAGE_NAME"
+        docker run --name "$CONTAINER_NAME" -d -p 0.0.0.0::5000 "$IMAGE_NAME"
+        HOST_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5000/tcp") 0).HostPort}}' "$CONTAINER_NAME")
+        echo "Container running on Host-Port: $HOST_PORT"
     else
         echo "No Dockerfile found in: $dir, skip..."
     fi
